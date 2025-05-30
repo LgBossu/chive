1cff2263e0a5147351cd701086b3a6c2ddb308ef
LgBossu <luigiduluco@gmail.com>
2025-05-30 19:35:28 +0200

    Fix type hint for hash_set_length function and clean up logger warning formatting

--- backend/utils/hash_utils.py
+++ backend/utils/hash_utils.py
@@ -12,7 +12,7 @@ def hash_message(message: str) -> str:
     return hashlib.sha256(message.encode("utf-8")).hexdigest()
 
 
-def hash_set_length(text: str, length: int = 8) -> str:
+def hash_set_length(text: bytes, length: int = 8) -> str:
     """
     Hash a string and return the first `length` characters.
     :param text: The text to hash -- encoded in UTF-8
@@ -37,8 +37,6 @@ def pad_int(number: int, length: int = 3) -> str:
     if length < 1:
         raise ValueError("The desired length should be greater than 0")
     elif len(str(number)) > length:
-        logger.warning(
-            f"The number {number} is longer than the desired length ({length})"
-        )
+        logger.warning(f"The number {number} is longer than the desired length ({length})")
         length = len(str(number))
     return str(number).zfill(length)
