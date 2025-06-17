aa09e6fb71b861f56554977e15f5eb3d477428f8
LgBossu <luigiduluco@gmail.com>
2025-06-18 00:36:34 +0200

    Working on logic for LegacyLinker implementation (to handle the proper legacy metafiles, which are CSV, until we migrate to SQL)

--- backend/loaders/metafiles_handlers.py
+++ backend/loaders/metafiles_handlers.py
@@ -80,3 +80,72 @@ class Linker(ABC):
         - The value is a list of tags associated with that message.
         """
         pass
+
+
+class LegacyLinker(Linker):
+    def __init__(self, legacy_db_path: str, legacy_metafiles_path: Tuple[str, str]):
+        # Legacy metafile is a pair of CSV files, not a proper database.
+        # first string is the path to the CSV file with tags, second is the blacklist
+        super().__init__(legacy_db_path)
+
+        self.legacy_metafiles_path = (
+            Path(legacy_metafiles_path[0]),
+            Path(legacy_metafiles_path[1]),
+        )
+
+        logger.debug(
+            f"Initializing LegacyLinker with db_path: {self.past_db_path} and metafiles: {self.legacy_metafiles_path}"  # noqa: E501
+        )
+
+        assert (
+            self.legacy_metafiles_path[0].exists() and self.legacy_metafiles_path[1].exists()
+        ), f"Legacy metafiles paths {self.legacy_metafiles_path} do not exist."
+
+    def get_taglist(self) -> np.ndarray:
+        """
+        Return the legacy taglist from metafiles as a numpy array.
+
+        The first column contains message IDs, and the second column contains tags.
+        If a message is blacklisted, it will have the tag "######" in the second column.
+
+        :return: A 2D numpy array with message IDs and tags.
+        :rtype: np.ndarray
+        """
+        logger.debug("Loading legacy taglist from metafiles")
+        legacy_taglist = []
+        with open(self.legacy_metafiles_path[0], "r") as tagfile:
+            csv_reader = reader(tagfile, delimiter=",")
+            for row in csv_reader:
+                legacy_taglist.append(row)
+        with open(self.legacy_metafiles_path[1], "r") as blacklistfile:
+            for line in blacklistfile:
+                legacy_taglist.append([line[:-1], BLACKLIST_TAG])
+                # Remove newline character and add a specific tag for blacklisted items
+
+        return np.array(legacy_taglist, dtype=str)
+
+    def link_past_tags(self) -> Dict[str, List[str]]:
+        """Link past tags to the past messages' document (raw text) content."""
+        logger.debug("Linking past tags to past messages")
+        taglist = self.get_taglist()
+        past_messages, _ = self.get_past_messages()
+
+        # Create a mapping from message content to tags
+        message_to_tags: Dict[str, List[str]] = dict()
+
+        for message, id_mess in past_messages:
+            message = str(message)
+            id_mess = str(id_mess)
+
+            if message not in message_to_tags:
+                message_to_tags[message] = []
+
+            # Find tags for the current message
+            tags_strings: List[str] = list(taglist[taglist[:, 0] == id_mess, 1].flatten())
+            all_tags = clean_tags(tags_strings)
+
+            # Add tags to the message
+            message_to_tags[message].extend(all_tags)
+
+        logger.debug(f"Linked tags to {len(message_to_tags)} messages")
+        return message_to_tags
