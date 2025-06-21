d08b6d43dbb4c5322495d8e18f083575596e2bf7
LgBossu <luigiduluco@gmail.com>
2025-06-22 00:15:22 +0200

    Add MessageTree class to represent a conversation message structure

---
+++ backend/models/message_tree.py
@@ -0,0 +1,9 @@
+from typing import Dict, List, Optional, Union
+
+from backend.models.message_node import MessageNode
+
+
+class MessageTree:
+    """A class representing a tree structure of messages in a conversation."""
+
+    pass
