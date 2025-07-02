af1c311c08e776ec1db4238773ec2913fb639001
LgBossu <luigiduluco@gmail.com>
2025-07-03 01:10:31 +0200

    Refactor update_db and categorize functions to accept log_path parameter; update API endpoint to use localhost

--- backend/app_actions.py
+++ backend/app_actions.py
@@ -1,23 +1,31 @@
+from pathlib import Path
+
 # from backend.processing.categorizer import CategorizerEngine
 from backend.loaders.chroma_endpoints import ChromaUpserter
+from backend.utils.log_setup import LoggerSetup
 
 
-def update_db():
+def update_db(log_path: Path):
     """
     Update the database contents.
 
     Runs the ChromaUpserter to add any missing conversations
     to the chroma database.
     """
+    # Set up logging
+    logger_setup = LoggerSetup()
+    logger_setup.configure_logger(force_log_file=log_path)
 
     # Upsert the categorized data into the database
-    chroma_upserter = ChromaUpserter(api_endpoint="http://localhost:8000/update_db_update")
+    chroma_upserter = ChromaUpserter(
+        api_endpoint="http://127.0.0.1:8000/update_db/update"
+    )  # TODO : do not hardcode endpoints
     chroma_upserter.upsert_all_conversations()
 
     return None
 
 
-def categorize():
+def categorize(log_path: Path):
     """
     Categorize messages in the database.
 
