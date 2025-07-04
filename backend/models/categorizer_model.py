da80210a6fa427fbf819b1061083f15e03d46aa1
LgBossu <luigiduluco@gmail.com>
2025-07-04 15:07:07 +0200

    Comment out tokenizer deletion in cleanup method and add TODO to investigate missing tokenizer in Categorizer0

--- backend/models/categorizer_model.py
+++ backend/models/categorizer_model.py
@@ -289,7 +289,8 @@ class CategorizerModel(ABC):
         The method should be overridden in subclasses to ensure proper cleanup of the cache.
         """
         logger.debug("Cleaning up the categorizer model and tokenizer.")
-        del self.tokenizer
+        # del self.tokenizer
+        # TODO : check why Categorizer0 does not have a self.tokenizer to delete
         del self.model
 
     @property
