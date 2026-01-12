1bad53e2cf1f7ee10a9c41e95dea0a6d54fa5b94
LgBossu <luigiduluco@gmail.com>
2026-01-12 19:29:19 +0100

    Enhance Engine class with configuration methods and connection management
    - factored into methods to set API endpoint, categorizer model, and connection wrappers.
    - reimplemented subprocess start method configuration with error handling.
    - Introduced Planner class for categorization statistics and message counting.
    - wrote a start method to call the subprocess and inject worker config from mother class attributes

--- backend/processing/categorizer.py
+++ backend/processing/categorizer.py
@@ -47,7 +47,12 @@ def display_time(seconds: float, tz: int = 1, duration: bool = False) -> str:
 
 
 class WorkerConfig(TypedDict):
-    pass
+    # Logging and API
+    log_file: Optional[Path]
+    api_endpoint: str
+
+    # Model type and wrapper classes
+    categorizer_model_type: type[CategorizerModel]
 
 class Worker:
     class Subclass:
@@ -63,12 +68,140 @@ def subprocess(worker_cls: type[Worker], config: WorkerConfig):
     worker = worker_cls(config)
     worker.run()
 
-class Engine:
-
-    def __init__(self) -> None:
-        pass
+class CategorizerEngine:
+    def set_io(self, api_endpoint: str) -> None:
+        self.api_url = api_endpoint
+        self.api_post_status = f"{self.api_url}/update"
+        self.api_get_status = f"{self.api_url}/status"
     
+    def set_categorizer_model(
+        self,
+        categorizer_model: Optional[type[CategorizerModel]],
+    ) -> None:
+        if categorizer_model is not None:
+            self.categorizer_model_type = categorizer_model
+        else:
+            self.categorizer_model_type = Categorizer0
+        logger.info(f"Setting categorizer model: {self.categorizer_model_type.__name__}")
+
+    def set_wrapper_classes(self) -> None:
+        self.metafile_writer_type = MetafileWriter
+        self.metafile_querier_type = MetafileQuerier
+        self.chroma_querier_type = ChromaQuerier
+    
+    def set_subprocess_start_method(self) -> None:
+        try:
+            set_start_method("spawn", force=True)
+        except RuntimeError:
+            logger.warning(
+                "Failed to set start method to 'spawn'. "
+                "This is likely because the start method has already been set."
+            )
+            assert (get_start_method() == "spawn"), ("The start method is not 'spawn'. Check configuration.")  # Debug this issue if it ever occurs
+        finally:
+            logger.info("Subprocess start method set to 'spawn'.")
+
+
+    def __init__(
+        self,
+        api_endpoint: str,
+        override_categorizer_model: Optional[type[CategorizerModel]] = None,
+        stalling_timeout: int = 45,
+        LOG_FILE: Optional[Path] = None,
+        CONSOLE_LOG_LEVEL: Optional[str] = "DEBUG",
+    ) -> None:
+        """
+        Initializes the categorizer engine.
+        This is a wrapper for the categorization loop.
+        """
+        self.set_io(api_endpoint)
+        self.ongoing_log_file = LoggerSetup.configure_logger(force_log_file=LOG_FILE, console_level=CONSOLE_LOG_LEVEL)
+        self.set_categorizer_model(override_categorizer_model)
+        self.set_wrapper_classes()
+        self.stalling_timeout = stalling_timeout
+        self.set_subprocess_start_method()
+
+
+    def open_connections(self) -> None:
+        """Instantiate connections to the databases"""
+        self.metafile_writer: MetafileWriter = self.metafile_writer_type()
+        self.metafile_querier: MetafileQuerier = self.metafile_querier_type()
+        self.chroma_querier: ChromaQuerier = self.chroma_querier_type()
+
+    def close_connections(self):
+        """Close connections to the databases"""
+        self.metafile_writer.close()
+        self.metafile_querier.close()
+        self.chroma_querier.close()
+
+    class Planner:
+        """
+        Establishes statistics, batch estimations, and other planning utilities for the categorization process.
+        """
+        def __init__(self,
+                     connection_wrappers: Tuple[ChromaQuerier, MetafileQuerier],
+                     ) -> None:
+            self.chroma_querier, self.metafile_querier = connection_wrappers
+        
+        def count_uncategorized_messages(self) -> DatabaseCounts:
+            """
+            Counts the number of uncategorized messages in the database.
+            Returns a tuple of (total count,non-empty uncategorized count, empty uncategorized count).
+            """
+            start_time = time()
+            logger.debug("Counting uncategorized messages in the database...")
+
+            total_uncategorized = 0
+            nonempty_uncategorized = 0
+            empty_uncategorized = 0
+
+            first_uncategorized_offset: Optional[int] = None
+
+            for batch in self.chroma_querier.stream_messages(include_metadata=True):
+                assert batch["metadatas"] is not None, "Metadatas weren't fetched." # Linter enforcement
+
+                categorized_ids = set(self.metafile_querier.match_ids(batch['ids']))
+                uncategorized_ids = [msg_id for msg_id in batch['ids'] if msg_id not in categorized_ids]
+
+                if first_uncategorized_offset is None and len(uncategorized_ids) > 0:
+                    first_uncategorized_offset = batch['ids'].index(uncategorized_ids[0])
+                
+                total_uncategorized += len(uncategorized_ids)
+                for msg_idx, msg_id in enumerate(batch['ids']):
+                    if msg_id in uncategorized_ids:
+                        is_empty = batch['metadatas'][msg_idx]['empty_or_non_text']
+                        if is_empty:
+                            empty_uncategorized += 1
+                        else:
+                            nonempty_uncategorized += 1
+            
+            elapsed = time() - start_time
+            logger.debug(f"Counted {total_uncategorized} uncategorized messages "
+                         f"({nonempty_uncategorized} non-empty, {empty_uncategorized} empty) in {display_time(elapsed, duration=True)}.")
+
+            return DatabaseCounts(
+                total_uncategorized = total_uncategorized,
+                nonempty_uncategorized = nonempty_uncategorized,
+                empty_uncategorized = empty_uncategorized,
+                first_uncategorized_offset = first_uncategorized_offset,
+            )
+
+    class Supervisor:
+        """
+        Supervises the categorization subprocess, monitoring its status and handling stalling detection.
+        """
+        pass
+
+
+    def configure_worker(self) -> WorkerConfig:
+        worker_config: WorkerConfig = {
+            "log_file": self.ongoing_log_file,
+            "api_endpoint": self.api_url,
+            "categorizer_model_type": self.categorizer_model_type,
+        }
+        return worker_config
+
     def start(self):
-        p = Process(target=subprocess, args=(Worker, WorkerConfig())) # spawned, not forked
+        p = Process(target=subprocess, args=(Worker, worker_config)) # spawned, not forked
         p.start()
         p.join()
