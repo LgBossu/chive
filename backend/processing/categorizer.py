b5fc493af52e9a88dad20f0ec6e48e596bceee74
LgBossu <luigiduluco@gmail.com>
2026-01-12 22:07:24 +0100

    Refactor Worker class to use ConnectionsWrapper and improve categorization process

--- backend/processing/categorizer.py
+++ backend/processing/categorizer.py
@@ -21,6 +21,8 @@ from backend.models.app_models import CategorizerJobInfo, JobStatus
 from backend.models.categorizer_model import Categorizer0, CategorizerModel
 from backend.utils.log_setup import LoggerSetup
 
+from chromadb.api.types import GetResult
+
 
 # TODO : maybe shard the databases and categorization process
 # to avoid memory issues when and if the project scales up.
@@ -107,18 +109,13 @@ class Worker:
             self.api_endpoint = api_endpoint
             self.last_update: Union[float, None] = None
 
-        def post_status_after_message(
+        def post_wrapper(
             self,
             eta: Optional[str] = None,
             current_message_id: Optional[str] = None,
             current_speed: Optional[float] = None,
-            # last_update: Optional[float] = None,
             processed_messages: int = 1,  # Increment processed messages by 1
         ) -> bool:
-            # TODO : add abort logic to the subprocess
-            # TODO : refactor the code below to use the API after every finished message.
-            # This will allow to update the job status and progress in real-time,
-            
             if self.last_update is None:
                 self.last_update = time()
 
@@ -147,6 +144,7 @@ class Worker:
             
             return False  # Do not abort the job if the connection fails
         
+
     class CategorizerModelWrapper:
         """
         Wraps the categorizer model for easy instantiation and usage.
@@ -166,6 +164,74 @@ class Worker:
             except self.model.ExceedingSafetyLimitError:
                 logger.warning(f"Message [{message_id}] exceeded safety limit. Assigning BLACKLIST_TAG.")
                 return [BLACKLIST_TAG]
+    
+    class ConnectionsWrapper:
+        """
+        Wraps the database connection classes for easy instantiation.
+        """
+        def __init__(
+            self,
+            metafile_writer_type: type[MetafileWriter],
+            metafile_querier_type: type[MetafileQuerier],
+            chroma_querier_type: type[ChromaQuerier],
+        ) -> None:
+            self.metafile_writer_type = metafile_writer_type
+            self.metafile_querier_type = metafile_querier_type
+            self.chroma_querier_type = chroma_querier_type
+        
+        def open_connections(self):
+            self.metafile_writer: MetafileWriter = self.metafile_writer_type()
+            self.metafile_querier: MetafileQuerier = self.metafile_querier_type()
+            self.chroma_querier: ChromaQuerier = self.chroma_querier_type()
+        
+        def close_connections(self):
+            self.metafile_writer.close()
+            self.metafile_querier.close()
+            self.chroma_querier.close()
+            del self.metafile_writer
+            del self.metafile_querier
+            del self.chroma_querier
+
+        def stream_messages(
+            self,
+            include_content: bool = True,
+            include_metadata: bool = True,
+            offset: int = 0,
+        ):
+            return self.chroma_querier.stream_messages(
+                include_content=include_content,
+                include_metadata=include_metadata,
+                offset=offset,
+            )
+
+        def identify_batch(self, message_ids: List[str]) -> List[int]:
+            """Given a batch of messages, identify which ones need categorization.
+            
+            :param message_ids: List of message IDs in the batch.
+            :return: A tuple containing a list of uncategorized message IDs and their indices in the batch.
+            """
+            assert self.metafile_querier is not None, "MetafileQuerier connection is not open."
+            
+            categorized_ids = set(self.metafile_querier.match_ids(message_ids))
+            uncategorized_idx = [idx for idx, msg_id in enumerate(message_ids) if msg_id not in categorized_ids]
+            return uncategorized_idx
+
+
+        def write_tagline(
+            self,
+            message_id: str,
+            categories: List[str],
+        ) -> None:
+            try:
+                self.metafile_writer.write_single_tagline(
+                    message_id=message_id,
+                    tags=categories,
+                    check_for_duplicates=True, # Should still not happen. Big red flag if it does.
+                )
+                logger.debug(f"Tags written for message {message_id}: {categories}")
+            except RuntimeError as e:
+                logger.critical(f"Failed to write tags for message {message_id}: {e}")
+                
 
     def __init__(self, config:WorkerConfig) -> None:
         LoggerSetup.configure_logger(force_log_file=config["log_file"])
@@ -175,39 +241,27 @@ class Worker:
         self.api_endpoint = config["api_endpoint"]
         self.api_messenger = self.APIMessenger(api_endpoint=self.api_endpoint)
         
-        self.categorizer_model_type = config["categorizer_model_type"]
+        self.categorizer_model_wrapper = self.CategorizerModelWrapper(model_type=config["categorizer_model_type"])
+        self.connections_wrapper = self.ConnectionsWrapper(
+            metafile_writer_type=config["metafile_writer_type"],
+            metafile_querier_type=config["metafile_querier_type"],
+            chroma_querier_type=config["chroma_querier_type"],
+        )
         
-        self.metafile_writer_type = config["metafile_writer_type"]
-        self.metafile_querier_type = config["metafile_querier_type"]
-        self.chroma_querier_type = config["chroma_querier_type"]
-
         self.database_counts = config["database_counts"]
 
         self.first_message: bool = True
 
+
     def open_connections(self):
-        """Instantiate connections to the databases"""
-        self.metafile_writer: MetafileWriter = self.metafile_writer_type()
-        self.metafile_querier: MetafileQuerier = self.metafile_querier_type()
-        self.chroma_querier: ChromaQuerier = self.chroma_querier_type()
+        self.connections_wrapper.open_connections()
     
     def close_connections(self):
-        """
-        Close connections to the databases, and delete the attributes
-        (connections need to be reinstantiated to be reopened).
-        """
-        self.metafile_writer.close()
-        self.metafile_querier.close()
-        self.chroma_querier.close()
-        del self.metafile_writer
-        del self.metafile_querier
-        del self.chroma_querier
+        self.connections_wrapper.close_connections()
+        del self.connections_wrapper
 
     def load_categorizer_model(self):
         """Instantiate the categorizer model."""
-        self.categorizer_model_wrapper = self.CategorizerModelWrapper(
-            model_type=self.categorizer_model_type,
-        )
         self.categorizer_model_wrapper.load_model()
     
     def close_categorizer_model(self):
@@ -215,53 +269,164 @@ class Worker:
         self.categorizer_model_wrapper.close_model()
         del self.categorizer_model_wrapper
 
-    def identify_batch(self, message_ids: List[str]) -> List[int]:
-        """Given a batch of messages, identify which ones need categorization.
-        
-        :param message_ids: List of message IDs in the batch.
-        :return: A tuple containing a list of uncategorized message IDs and their indices in the batch.
-        """
-        assert self.metafile_querier is not None, "MetafileQuerier connection is not open."
-        
-        categorized_ids = set(self.metafile_querier.match_ids(message_ids))
-        uncategorized_idx = [idx for idx, msg_id in enumerate(message_ids) if msg_id not in categorized_ids]
-        return uncategorized_idx
 
-    def categorize_message_batch(self, messages: List[Tuple[str, str, bool]]):
-        """Given a batch's subset of uncategorized messages, categorize them."""
+    def initialize_loops(self):
+        logger.info("Initializing categorization loops and opening connections...")
+        
+        self.open_connections()
+        self.load_categorizer_model()
+
+        self.starting_time = time()
+        self.last_checked_time = self.starting_time
+        self.processing_times = []
+        self.total_processed = 0
+        self.llm_processed = 0
+
+        logger.info("Initialization complete. Resources opened.")
+    
+    def end_loops(self):
+        logger.info("Closing categorization loops and freeing resources...")
+        self.close_categorizer_model()
+        self.close_connections()
+        logger.info("Resources closed.")
+
+    def console_log_progress(self):
+        cur_time = time()
+        average_processing_time = (
+            sum(self.processing_times) / len(self.processing_times) if self.processing_times else 0
+        )  # noqa: E501
+        average_processing_time = int(average_processing_time * 100) / 100
+        logger.info(
+            f"Time since start: {display_time(cur_time - self.starting_time, duration=True)}"  # noqa: E501
+        )
+        logger.info(
+            f"Examined  : {self.total_processed} messages out of {self.database_counts['total_uncategorized']} ({int(self.total_processed/self.database_counts['total_uncategorized']*10000)/100}%)"  # noqa: E501
+        )
+        logger.info(
+            f"Processed : {self.llm_processed} messages"  # noqa: E501
+        )
+        logger.info(
+            f"Average processing time: {average_processing_time} seconds"  # noqa: E501
+        )
+        logger.info(
+            f"ETA: {display_time(average_processing_time * (self.database_counts["nonempty_uncategorized"] - self.llm_processed), duration=True)}"  # noqa: E501
+        )
+        self.last_checked_time = cur_time
+
+
+    def categorize_message_batch(self, messages: List[Tuple[str, str, bool]]) -> bool:
+        """Given a batch's subset of uncategorized messages, categorize them.
+        
+        :return: True if the process was aborted, False otherwise.
+        """
+        abort = False
+
         for message_id, message_content, is_empty in messages:
+            if self.first_message:
+                abort = self.api_messenger.post_wrapper(processed_messages=0, current_message_id=message_id)
+                self.first_message = False
+            else:
+                average_processing_time = sum(self.processing_times) / len(self.processing_times) if self.processing_times else 0.0
+                abort = self.api_messenger.post_wrapper(
+                    eta=display_time(
+                        average_processing_time * (self.database_counts["total_uncategorized"] - self.total_processed),
+                        duration=True,
+                    ),
+                    current_message_id=message_id,
+                    current_speed=average_processing_time,
+                )
+
+            if abort:
+                break
+
             if is_empty:
                 categories = [EMPTY_TAG]
+                self.total_processed += 1
             else:
+                start_time = time()
                 categories = self.categorizer_model_wrapper.categorize(message_id, message_content)
+                end_time = time()
+                self.processing_times.append(end_time - start_time)
+                self.total_processed += 1
+                self.llm_processed += 1
+                # Keep processing times list to last 100 entries to avoid memory issues
+                if len(self.processing_times) > 100:
+                    self.processing_times = self.processing_times[-100:]
             # TODO : write tagline to database and continue code
 
+            self.connections_wrapper.write_tagline(
+                message_id=message_id,
+                categories=categories,
+            )
+            if time() - self.last_checked_time > 60*3:
+                # Console log every 3 minutes
+                self.console_log_progress()
 
-    def run(self):
-        """Main categorization loop."""
-        self.open_connections()
+        return abort
 
-        for n, batch in enumerate(self.chroma_querier.stream_messages(include_content=True, include_metadata=True)): # TODO : after merge, account for offset
+    def process_batch(self, batch: GetResult) -> bool:
+        """Process a batch of messages from the database.
+        
+        :return: True if the process was aborted, False otherwise.
+        """
+        assert batch["documents"] is not None, "Documents weren't fetched." # Linter enforcement
+        assert batch["metadatas"] is not None, "Metadatas weren't fetched." # Linter enforcement
+        uncategorized_idx = self.connections_wrapper.identify_batch(batch["ids"])
+
+        uncategorized_messages: List[Tuple[str, str, bool]] = []
+        for idx in uncategorized_idx:
+            message_id, message_content = batch["ids"][idx], batch["documents"][idx]
+            is_empty = batch["metadatas"][idx].get("empty_or_non_text", None)
+            assert is_empty is not None and isinstance(is_empty, bool), "Metadata missing 'empty_or_non_text' flag."
+            uncategorized_messages.append((message_id, message_content, is_empty))
+
+        abort = self.categorize_message_batch(uncategorized_messages)
+        return abort
+
+    def run(self) -> bool:
+        """Main categorization loop.
+        
+        :return: True if the process was aborted, False otherwise.
+        """
+        if self.database_counts["total_uncategorized"] is None:
+            # TODO : handle differently ?
+            logger.info("No uncategorized messages found. Exiting categorization loop.")
+            self.close_connections()
+            return False
+        assert self.database_counts["first_uncategorized_offset"] is not None # For mypy linter
+
+        self.initialize_loops()
+
+        abort = False
+        for n, batch in enumerate(
+            self.connections_wrapper.stream_messages(
+                include_content=True,
+                include_metadata=True,
+                offset=self.database_counts["first_uncategorized_offset"],
+                )
+            ):
             logger.debug(f"Processing batch (number {n})...")
-            assert batch["documents"] is not None, "Documents weren't fetched." # Linter enforcement
-            assert batch["metadatas"] is not None, "Metadatas weren't fetched." # Linter enforcement
-            uncategorized_idx = self.identify_batch(batch["ids"])
+            abort = self.process_batch(batch)
+            if abort:
+                break
 
-            uncategorized_messages: List[Tuple[str, str, bool]] = []
-            for idx in uncategorized_idx:
-                message_id, message_content = batch["ids"][idx], batch["documents"][idx]
-                is_empty = batch["metadatas"][idx].get("empty_or_non_text", None)
-                assert is_empty is not None and isinstance(is_empty, bool), "Metadata missing 'empty_or_non_text' flag."
-                uncategorized_messages.append((message_id, message_content, is_empty))
-
-            self.categorize_message_batch(uncategorized_messages)
-
-        self.close_connections()
-
+        if abort:
+            logger.warning("Abort signal received from API. Exiting categorization process. Closing resources...")
+        
+        self.end_loops()
+        logger.info("Resources closed. Exiting categorization process.")
+        return abort
 
 def subprocess(worker_cls: type[Worker], config: WorkerConfig):
     worker = worker_cls(config)
-    worker.run()
+    abort = worker.run()
+
+    # TODO :resource freeing necessary ?
+
+    if abort:
+        worker.signal_handler.abort_exit()
+    else:
+        worker.signal_handler.finished_exit()
 
 class CategorizerEngine:
     def set_io(self, api_endpoint: str) -> None:
